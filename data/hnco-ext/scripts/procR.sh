ac67131c9a7de953202f5455a11615108f144287
Bradley Worley <geekysuavo@gmail.com>
2016-01-25 11:02:16 -0600

    added full logging to all example processing scripts

--- data/hnco-ext/scripts/procR.sh
+++ data/hnco-ext/scripts/procR.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 xyz2pipe -in ../data/yzx/hnco%04d.nus -x  | \
-  ../../../bin/camera   \
-    -sched ../nuslist   \
-    -dims 2             \
-    -xN 128             \
-    -yN 256             \
-    -iters 100          \
-    -threads 8        | \
+  ../../../bin/camera     \
+    -sched ../nuslist     \
+    -log ../data/hnco.log \
+    -dims 2               \
+    -xN 128               \
+    -yN 256               \
+    -iters 100            \
+    -threads 8          | \
   pipe2xyz -out ../data/yzx/hnco%04d.ft1 -x -ov
 
 times
--- data/noesy/scripts/procR.sh
+++ data/noesy/scripts/procR.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 xyz2pipe -in ../data/yzax/noe%04d%04d.nus -x  | \
-  ../../../bin/camera  \
-    -sched ../nuslist  \
-    -dims 3            \
-    -xN 64             \
-    -yN 64             \
-    -zN 64             \
-    -iters 200         \
-    -delta 1           \
-    -sigma 10          \
-    -threads 8       | \
+  ../../../bin/camera    \
+    -sched ../nuslist    \
+    -log ../data/noe.log \
+    -dims 3              \
+    -xN 64               \
+    -yN 64               \
+    -zN 64               \
+    -iters 200           \
+    -delta 1             \
+    -sigma 10            \
+    -threads 8         | \
   pipe2xyz -out ../data/yzax/noe%04d%04d.ft1 -x -verb -ov
 
 times
